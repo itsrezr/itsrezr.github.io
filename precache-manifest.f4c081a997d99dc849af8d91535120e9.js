c0c252b6468891d32cb9320108d9e1e6b115a033
= <rezvan244@hotmail.co.uk>
2021-06-28 18:56:43 -0700

    Updates

---
+++ precache-manifest.f4c081a997d99dc849af8d91535120e9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb079d418b49c70d41ca4ec4997fb4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ce6784efdc7680d49ad",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "af916c9e2ffea29660e3",
+    "url": "/static/css/main.b1c9947e.chunk.css"
+  },
+  {
+    "revision": "6ce6784efdc7680d49ad",
+    "url": "/static/js/2.eea54a25.chunk.js"
+  },
+  {
+    "revision": "caa799b2195062f13c84e9fdfdd6b866",
+    "url": "/static/js/2.eea54a25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af916c9e2ffea29660e3",
+    "url": "/static/js/main.228e3e50.chunk.js"
+  },
+  {
+    "revision": "6ffd81d391f4a8bc0b58",
+    "url": "/static/js/runtime-main.2c6c115a.js"
+  },
+  {
+    "revision": "c8076e0385265f440f8d5e5d136b7622",
+    "url": "/static/media/AboutMePic.c8076e03.jpeg"
+  },
+  {
+    "revision": "22dae37ef667416b3b685632cfe1c93a",
+    "url": "/static/media/ChatBotChat.22dae37e.png"
+  },
+  {
+    "revision": "c919b2b0aede8afa4f5ceeeb5cd9d6e3",
+    "url": "/static/media/ChatBotIcon.c919b2b0.png"
+  },
+  {
+    "revision": "94274fe5fc4b8baaeb9ba533e4064f54",
+    "url": "/static/media/Companies.94274fe5.png"
+  },
+  {
+    "revision": "709ffa136368895a9ccd8847ed0572d4",
+    "url": "/static/media/DesignBackground2.709ffa13.jpg"
+  },
+  {
+    "revision": "4197e7e7902fc3127b3431ad477d8111",
+    "url": "/static/media/Payment.4197e7e7.png"
+  },
+  {
+    "revision": "0e8de080f1927040e8277221e5276bb5",
+    "url": "/static/media/PillUpDemo.0e8de080.mp4"
+  },
+  {
+    "revision": "43066bb0eaf362556e0ae97a411a415f",
+    "url": "/static/media/PlanetPanicDemo.43066bb0.mp4"
+  },
+  {
+    "revision": "112c47d2ae5a3349467e123dbdc80f8c",
+    "url": "/static/media/Planet_Panic_MainPic.112c47d2.png"
+  },
+  {
+    "revision": "d94189b37c40d13c4a7dca28f3b5ca5d",
+    "url": "/static/media/heavydatacorplogo.d94189b3.png"
+  },
+  {
+    "revision": "525d163ede36bfccf0e7beac8031b7c3",
+    "url": "/static/media/maingamepicture.525d163e.jpg"
+  },
+  {
+    "revision": "a9f3707cc0f3389d3b8b32c53745bc57",
+    "url": "/static/media/portrait_logo_rez_with_paint1.a9f3707c.png"
+  },
+  {
+    "revision": "f91033dcbda07132f76de538294e2499",
+    "url": "/static/media/rezzmix.f91033dc.JPG"
+  },
+  {
+    "revision": "e0524739dd02007367d99e1055e191e1",
+    "url": "/static/media/toDoList.e0524739.png"
+  },
+  {
+    "revision": "fd068e91ef5a464e85cfb2eec2ddb357",
+    "url": "/static/media/ubctalentsized.fd068e91.jpg"
+  },
+  {
+    "revision": "7ea7726ac3197a89fc2ac17d7075f476",
+    "url": "/static/media/ubcwomeninsciencelogo_byRez.7ea7726a.jpg"
+  }
+]);
