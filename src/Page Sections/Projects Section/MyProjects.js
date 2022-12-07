4155f06d882ad784b2736a30769a22c8a7440a2e
= <60489397+rezriazi@users.noreply.github.com>
2022-12-06 22:07:49 -0800

    edited project descriptions

--- src/Page Sections/Projects Section/MyProjects.js
+++ src/Page Sections/Projects Section/MyProjects.js
@@ -79,29 +79,28 @@ const Projects = (props) => {
         title="The Office: Somehow We Manage"
         content={
           <>
+            <p>East Side Games, Jan - Sept 2021</p>
             <p>
-            In January 2021 I joined <b>East Side Games</b> as the third <b>Software Engineer</b> during the pre-production 
+            I joined as the third <b>Software Engineer</b> during the pre-production 
             of the game for the show <b>The Office</b>, in collaboration with NBC and Universal Studios. I was 
-            one of eight developers to create the mobile game <b>The Office: Somehow We Manage</b>, which is released January 2022.
+            one of eight developers to create the mobile game <b>The Office: Somehow We Manage.</b>
             </p>
             <p>
-            I was independently responsible for <b>implementing all audio</b> in the game (music, 
-            sound effects, ambience, stings, etc.). More notably, I was given <b>my own 
-            feature</b> in the game in which I created with full autonomy. I developed the ascension 
-            feature of the game -- a challenge the user plays after they complete an episode. Working 
-            with <b>C#</b> and <b>Unity</b>, I created service classes, load phases, algorithms, and 
+            I was responsible for <b>implementing all audio</b> in the game (music, 
+            sound effects, ambience, stings, etc.). I was also given <b>my own 
+            feature</b> in the game in which I created with full autonomy. I also developed the ascension 
+            feature (The Boss Challenge: a mini game after an episode is completed) of the game with full autonomy.
+            </p>
+            <p>
+            Mainly developed in <b>C#</b> and <b>Unity</b>, I created service classes, load phases, algorithms, and 
             helped develop a "saving data" architecture.
             </p>
             <p>
-            As an intern, I was treated and had the same responsibilities 
-            as a normal software engineer. I helped <b>plan sprints, attended all engineer meetings, 
-            managed my own features</b>, and most importantly, was part of the <b>code review process</b> where I 
-            was responsible for <b>reviewing and approving other developer's code</b>. I was in consistent 
-            collaboration and in sync with all disciplines of the team -- such as production, story 
-            writers, UI/UX, artists, game designers, QA, etc. 
+            Responsibilities include helping <b>plan sprints, managing my own features</b>, and <b>reviewing and approving
+            other Engineers' codes</b>. In consistent collaboration with production, story writers, UI/UX, artists, game designers, QA, etc. 
             </p>
             <p>
-              The game is available on iOS/Android, and has over a million downloads.&nbsp;
+              The game is available on iOS/Android, with 1M+ downloads.&nbsp;
               <a
                 href="https://play.google.com/store/apps/details?id=com.eastsidegames.theofficeswm&hl=en&gl=US"
                 target="_blank"
@@ -122,6 +121,7 @@ const Projects = (props) => {
         title="www.rezriazi.com"
         content={
           <>
+            <p>May 2020.</p>
             <p>
               I coded this website using <b>React</b> with languages <b>JavaScript, 
               HTML, and CSS</b>. It is implemented through <b>Node.js</b> in <b>Microsoft 
@@ -135,15 +135,15 @@ const Projects = (props) => {
         id="PillUpModal"
         title="Pill Up - Interactive Arcade Game"
         content={
-          <>
+          <> 
+            <p>48-hour BC Game Jam, Feb 2020</p>
             <p>
               Fun and addictive game made using Java in IntelliJ with JavaFX.
               The objective of the game is to survive as long as possible, by
               avoiding spikes, aging pills, and growth pills. This was a team
               project, and in addition to coding, I was responsible for the User
               Interface, User Experience Design, creating the graphics (made in
-              Krita), and debugging. This was made at the 48-hour BC Game Jam in 
-              Febuary 2020.
+              Krita), and debugging.
             </p>
             <p>
               Use Left/Right/Up/Down arrows to move & SPACE bar to pause game
@@ -176,13 +176,11 @@ const Projects = (props) => {
         title="Planet Panic - Educational Kids Game"
         content={
           <>
+            <p>Hacking For Humanity Hackathon, May 2019</p>
             <p>
-              At the 2019 Hacking For Humanity Hackathon hosted by Girls in Tech, I
-              made the game “Planet-Panic”. The objective of the game is to
-              collect different pieces of garbage and put it in the bin it
-              belongs to, which educates kids about waste. I created all of the
-              graphics and User Interface Design using photoshop environment,
-              and the game was implemented in Python.
+              The objective of the game is to collect different pieces of garbage and put it
+              in the bin it belongs to, which educates kids about waste. I created the
+              graphics and User Interface Design using Photoshop, and the game was implemented in Python.
             </p>
             <div className="row">
               <video
@@ -200,6 +198,7 @@ const Projects = (props) => {
         title="To Do List"
         content={
           <>
+            <p>Nov 2019</p>
             <p>
               Created a To-Do List Application using Java in IntelliJ IDEA with
               JUnit testing and GUI as an academic project. Specific abstract
@@ -215,13 +214,12 @@ const Projects = (props) => {
         title="Live Chat Bot"
         content={
           <>
+            <p>Lifco Hydraulics, Sept - Dec 2020</p>
             <p>
-              Working as a Full Stack Software Developer at Lifco Hydraulics, 3 other Co-op students and I created a
+              Working as a Full Stack Software Developer Co-op, 3 other Co-op students and I created a
               Live Chat Bot from scratch using C#, .Net, and Microsoft Azure Bot. The goal of the project was to create a 
-              live connection between employees and customers. The team included 2 Back-End Developers, 1 Front-End
-              Developer, and a Product Researcher/Database Developer. My role was being the Front-End Software Developer as 
-              well as help developing some back-end tools such as passing the user's information (their URL, IP Address, etc.) 
-              to the employee in the chat.
+              live connection between employees and customers. My role was more Front-End, but also helped develop some
+              back-end API connecting user information to the employee, such as their URL, IP Address, etc.
               </p>
               <div className="row">
               <img src={chatBotChat}/>
@@ -235,14 +233,13 @@ const Projects = (props) => {
         title="Employee Software Tools"
         content={
           <>
+            <p>Lifco Hydraulics, Sept - Dec 2020</p>
             <p>
-              As a Full Stack Software Developer Co-op at Lifco Hydraulics, I created many softwares for Sales
+              As a Full Stack Software Developer Co-op, I created many softwares for Sales
               employees to use. This includes a payment software for employees to enter clients' card details to 
               process sales, a Viewer for all of the companies that are registered customers, a Viewer that
               displays all recent Cash Receipts, etc. Technologies used were Javascript, HTML/CSS for developing the 
               website, C# as the programming language, and SQL and MongoDB as databases to retrieve and push data.
-              </p><p>
-              (Image Credit Card details are simulated)
               </p>
               <div className="row">
               <img src={paymentDemo}/>
